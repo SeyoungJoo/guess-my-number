2621ddba34ff6034983afc56699851c8891a244b
seyoungjoo <sellyjphoto@gmail.com>
2020-12-27 03:45:47 +0900

    game score js logic

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const secretNumber = Math.trunc(Math.random()*20)+1; //decimal number between 1 to 20
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function(){
   const guess = Number(document.querySelector('.guess').value);
@@ -11,8 +10,8 @@ document.querySelector('.check').addEventListener('click', function(){
     document.querySelector('.message').textContent = "❌ No Number!";
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = "🎉 Corret Number!";
-    score++;
-    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = secretNumber;
+
     document.querySelector('body').style.backgroundColor = '#583d72';
     document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
